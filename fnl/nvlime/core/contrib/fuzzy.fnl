1049897f36693e8102c09614c3dbf57eaf8e92aa
Ictye(jj) <northgreen2006@qq.com>
2026-06-11 23:55:35 +0800

    fix(fuzzy): replace self:method with connection.method self to avoid metatable lookup
    connection objects stored in vim.b.nvlime_conn lose their metatable (__index).
    Using self:method syntax caused "attempt to call field 'get-current-package' (a nil value)"
    because the __index table was not available for method lookup.
    
    Fix: call methods directly via connection module:
    - self:get-current-package → connection.get-current-package self
    - self:send → connection.send self
    - self:emacs-rex → connection.emacs-rex self
    - self:simple-send-cb → connection.simple-send-cb self callback ...

--- fnl/nvlime/core/contrib/fuzzy.fnl
+++ fnl/nvlime/core/contrib/fuzzy.fnl
@@ -6,14 +6,14 @@
 (fn connection.fuzzy-completions [self symbol ?callback]
   "Get fuzzy completion list for SYMBOL in the current package.
   Returns results via ASYNC callback: (callback self results)."
-  (let [cur-package (let [pkg-info (self:get-current-package)]
+  (let [cur-package (let [pkg-info (connection.get-current-package self)]
                       (if pkg-info
                           (. pkg-info 1)
                           nil))]
-    (self:send (self:emacs-rex
+    (connection.send self (connection.emacs-rex self
                  [(connection.sym "SWANK" "FUZZY-COMPLETIONS") symbol cur-package])
                (fn [chan msg]
-                 (self:simple-send-cb callback "nvlime#contrib#fuzzy#FuzzyCompletions" chan msg)))))
+                 (connection.simple-send-cb self callback "nvlime#contrib#fuzzy#FuzzyCompletions" chan msg)))))
 
 (fn connection.init-fuzzy [self]
   "Register fuzzy completion method on connection object."
--- lua/nvlime/core/contrib/fuzzy.lua
+++ lua/nvlime/core/contrib/fuzzy.lua
@@ -2,7 +2,7 @@ local connection = require("nvlime.core.connection")
 connection["fuzzy-completions"] = function(self, symbol, _3fcallback)
   local cur_package
   do
-    local pkg_info = self["get-current-package"](self)
+    local pkg_info = connection["get-current-package"](self)
     if pkg_info then
       cur_package = pkg_info[1]
     else
@@ -10,9 +10,9 @@ connection["fuzzy-completions"] = function(self, symbol, _3fcallback)
     end
   end
   local function _2_(chan, msg)
-    return self["simple-send-cb"](self, callback, "nvlime#contrib#fuzzy#FuzzyCompletions", chan, msg)
+    return connection["simple-send-cb"](self, callback, "nvlime#contrib#fuzzy#FuzzyCompletions", chan, msg)
   end
-  return self:send(self["emacs-rex"](self, {connection.sym("SWANK", "FUZZY-COMPLETIONS"), symbol, cur_package}), _2_)
+  return connection.send(self, connection["emacs-rex"](self, {connection.sym("SWANK", "FUZZY-COMPLETIONS"), symbol, cur_package}), _2_)
 end
 connection["init-fuzzy"] = function(self)
   self["FuzzyCompletions"] = connection["fuzzy-completions"]
